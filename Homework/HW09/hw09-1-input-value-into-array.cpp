79a676ab565bde0903aa98b11a3ecb3dfe74e4fd
porameter <paramet_su66@live.rmutl.ac.th>
2023-09-20 21:56:05 +0700

    update hw09-1

--- Homework/HW09/hw09-1-input-value-into-array.cpp
+++ Homework/HW09/hw09-1-input-value-into-array.cpp
@@ -25,3 +25,37 @@
         Array = 1 1 3 9 8 2 7 8
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int N, i ;
+    
+    printf( "ป้อนจำนวน N: " ) ;
+    scanf( "%d" , &N ) ;
+
+    int arr[N] ;
+
+    for ( i = 0; i < N; i++ ) {
+        printf( "Input [%d] : " , i ) ;
+        scanf( "%d" , &arr[i] ) ;
+
+        // ตรวจสอบว่าผู้ใช้ป้อนค่า -1 หรือไม่
+        if ( arr[i] == -1 ) {
+            N = i ; // ปรับขนาดของอาร์เรย์เพื่อไม่นับค่า -1
+            break ; // ออกจากลูป
+        }
+    }
+    printf( "Index = " ) ;
+    for ( i = 0; i < N; i++ ) {
+        printf( "%d " , i ) ;
+    }
+    printf( "\nArray = " ) ;
+    for ( i = 0; i < N; i++ ) {
+        printf( "%d " , arr[i] ) ;
+    }
+    printf( "\n" ) ;
+
+    return 0 ;
+}
+
